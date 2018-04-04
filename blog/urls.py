467de7a6d71b6dec34a0d7724c3f41a0816dc087
isac21 <isac31@naver.com>
2018-04-04 16:29:55 +0900

    sed

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,6 @@ from blog import views
 
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'Django_hello.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
     url(r'^$', views.index, name='detail'),
 ]
